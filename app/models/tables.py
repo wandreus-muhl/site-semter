3370c7ddedd8fa0b630a7bb71a716146b4c8a408
Sharkovisky <whiteaxefun@gmail.com>
2021-04-17 00:37:33 -0400

    Todos os models adicionados

--- app/models/tables.py
+++ app/models/tables.py
@@ -18,7 +18,7 @@ class Pessoa(db.Model, UserMixin):
 
 
 class Servidor(db.Model, UserMixin):
-    _tablename_ = "servidores"
+    __tablename__ = "servidores"
 
     id = db.Column(db.Integer, primary_key=True)
     matricula = db.Column(db.String(10), nullable=False, index=True, unique=True)
@@ -27,3 +27,42 @@ class Servidor(db.Model, UserMixin):
 
     def _repr_(self):
         return "<Servidor %d>" % self.matricula
+
+class Contribuinte(db.Model):
+    __tablename__ = "contribuintes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    cpf = db.Column(db.String(20), nullable=False, index=True, unique=True)
+    cnpj = db.Column(db.String(45))
+    pessoa_id = db.Column(db.Integer, db.ForeignKey("pessoas.id"), nullable=False)
+
+    def _repr_(self):
+        return "<Contribuinte %d>" % self.cpf
+
+class Status(db.Model):
+    __tablename__ = "status"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(250), nullable=False)
+    descricao = db.Column(db.String(250), nullable=False)
+    data_atualizacao = db.Column(db.DateTime, nullable=False)
+
+    def _repr_(self):
+        return "<Status tipo %s>" % self.nome
+
+class Processo(db.Model):
+    __tablename__ = "processos"
+
+    id = db.Column(db.Integer, primary_key=True)
+    cod_processo = db.Column(db.String(45), nullable=False, index=True, unique=True)
+    descricao = db.Column(db.String(250))
+    tipo_processo = db.Column(db.String(250), nullable=False)
+    data_inicio = db.Column(db.DateTime, nullable=False)
+    data_final = db.Column(db.DateTime, nullable=False)
+    parecer = db.Column(db.String(250))
+    servidor_id = db.Column(db.Integer, db.ForeignKey("servidores.id"), nullable=False)
+    contribuinte_id = db.Column(db.Integer, db.ForeignKey("contribuintes.id"), nullable=False)
+    status_id = db.Column(db.Integer, db.ForeignKey("status.id"), default="0") #Quando tiver o status certo de encaminhado, colocar aqui!
+
+    def _repr_(self):
+        return "<Processo %d>" % self.cod_processo
